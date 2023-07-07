f4c22a084e772d741b7dd537f2e979818aafd771
klik-on <123057191+klik-on@users.noreply.github.com>
2023-07-07 09:09:41 +0700

    Update updt_tomcat.sh

--- arcgis-server/patch/updt_tomcat.sh
+++ arcgis-server/patch/updt_tomcat.sh
@@ -11,7 +11,7 @@ cp -r lib lib_bak
 
 cp -fv /tmp/apache-tomcat-9.0.76/bin/*.jar /home/arcgis/server/framework/runtime/tomcat/bin/
 cp -fv /tmp/apache-tomcat-9.0.76/lib/*.jar /home/arcgis/server/framework/runtime/tomcat/lib/
- /home/arcgis/server/framework/runtime/tomcat/bin/version.sh
+/home/arcgis/server/framework/runtime/tomcat/bin/version.sh
 
 /home/arcgis/server/startserver.sh
 
