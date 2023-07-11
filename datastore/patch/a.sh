1f6c805846191569770f9591accf2a866a53a496
klik-on <123057191+klik-on@users.noreply.github.com>
2023-07-11 07:31:50 +0700

    Update a.sh

--- datastore/patch/a.sh
+++ datastore/patch/a.sh
@@ -1,6 +1,10 @@
 # buat volume docker =>  mkdir -p data/{config-store,directories,logs,sysgen}
 # docker cp patch DATASTORE:/tmp
 
+# cara update os
+# docker exec -it -u root DATASTORE /bin/bash
+# apt update && apt upgrade
+
 echo My hostname is $HOSTNAME
 
 for patchfile in *.tar; do
