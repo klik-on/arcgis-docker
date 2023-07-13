5ffa8de05d7ba1c7999dd95df2aae4a7eac1b029
klik-on <123057191+klik-on@users.noreply.github.com>
2023-07-13 08:19:26 +0700

    Update updt_tomcat.sh

--- arcgis-server/patch/updt_tomcat.sh
+++ arcgis-server/patch/updt_tomcat.sh
@@ -1,18 +1,19 @@
  /home/arcgis/server/framework/runtime/tomcat/bin/version.sh
 
+tver='9.0.78'
 cd /tmp
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.78/bin/apache-tomcat-9.0.78.tar.gz
-tar -xvf apache-tomcat-9.0.78.tar.gz
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v$tver/bin/apache-tomcat-$tver.tar.gz
+tar -xvf apache-tomcat-$tver.tar.gz
 
 /home/arcgis/server/stopserver.sh
 cd /home/arcgis/server/framework/runtime/tomcat/
 cp -r bin bin_bak
 cp -r lib lib_bak
 
-cp -fv /tmp/apache-tomcat-9.0.78/bin/*.jar /home/arcgis/server/framework/runtime/tomcat/bin/
-cp -fv /tmp/apache-tomcat-9.0.78/lib/*.jar /home/arcgis/server/framework/runtime/tomcat/lib/
+cp -fv /tmp/apache-tomcat-$tver/bin/*.jar /home/arcgis/server/framework/runtime/tomcat/bin/
+cp -fv /tmp/apache-tomcat-$tver/lib/*.jar /home/arcgis/server/framework/runtime/tomcat/lib/
 /home/arcgis/server/framework/runtime/tomcat/bin/version.sh
 
 /home/arcgis/server/startserver.sh
 
-rm -rf /tmp/apache-tomcat-9*
+rm -rf /tmp/apache-tomcat-$tver*
