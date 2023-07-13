37e6eb63e097d048d0ff10959739d82f197db10c
klik-on <123057191+klik-on@users.noreply.github.com>
2023-07-13 07:48:03 +0700

    Update updt_tomcat.sh

--- datastore/patch/updt_tomcat.sh
+++ datastore/patch/updt_tomcat.sh
@@ -1,16 +1,16 @@
  /home/arcgis/datastore/framework/runtime/tomcat/bin/version.sh
 
 cd /tmp
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.76/bin/apache-tomcat-9.0.76.tar.gz
-tar -xvf apache-tomcat-9.0.76.tar.gz
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.78/bin/apache-tomcat-9.0.78.tar.gz
+tar -xvf apache-tomcat-9.0.78.tar.gz
 
 /home/arcgis/datastore/stopdatastore.sh
 cd /home/arcgis/datastore/framework/runtime/tomcat/
 cp -r bin bin_bak
 cp -r lib lib_bak
 
-cp -fv /tmp/apache-tomcat-9.0.76/bin/*.jar /home/arcgis/datastore/framework/runtime/tomcat/bin/
-cp -fv /tmp/apache-tomcat-9.0.76/lib/*.jar /home/arcgis/datastore/framework/runtime/tomcat/lib/
+cp -fv /tmp/apache-tomcat-9.0.78/bin/*.jar /home/arcgis/datastore/framework/runtime/tomcat/bin/
+cp -fv /tmp/apache-tomcat-9.0.78/lib/*.jar /home/arcgis/datastore/framework/runtime/tomcat/lib/
 /home/arcgis/datastore/framework/runtime/tomcat/bin/version.sh
 
 /home/arcgis/datastore/startdatastore.sh
