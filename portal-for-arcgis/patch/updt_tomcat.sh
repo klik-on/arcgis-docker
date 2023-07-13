72328190e94abcd0b4616df361283ddc400f37e1
klik-on <123057191+klik-on@users.noreply.github.com>
2023-07-13 08:24:08 +0700

    Update updt_tomcat.sh

--- portal-for-arcgis/patch/updt_tomcat.sh
+++ portal-for-arcgis/patch/updt_tomcat.sh
@@ -1,18 +1,19 @@
  /home/arcgis/portal/framework/runtime/tomcat/bin/version.sh
 
+tver='9.0.78'
 cd /tmp
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.78/bin/apache-tomcat-9.0.78.tar.gz
-tar -xvf apache-tomcat-9.0.78.tar.gz
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v$tver/bin/apache-tomcat-$tver.tar.gz
+tar -xvf apache-tomcat-$tver.tar.gz
 
 /home/arcgis/portal/stopportal.sh
 cd /home/arcgis/portal/framework/runtime/tomcat/
 cp -r bin bin_bak
 cp -r lib lib_bak
 
-cp -fv /tmp/apache-tomcat-9.0.78/bin/*.jar /home/arcgis/portal/framework/runtime/tomcat/bin/
-cp -fv /tmp/apache-tomcat-9.0.78/lib/*.jar /home/arcgis/portal/framework/runtime/tomcat/lib/
+cp -fv /tmp/apache-tomcat-$tver/bin/*.jar /home/arcgis/portal/framework/runtime/tomcat/bin/
+cp -fv /tmp/apache-tomcat-$tver/lib/*.jar /home/arcgis/portal/framework/runtime/tomcat/lib/
 /home/arcgis/portal/framework/runtime/tomcat/bin/version.sh
 
 /home/arcgis/portal/startportal.sh
 
-rm -rf /tmp/apache-tomcat-9*
+rm -rf /tmp/apache-tomcat-$tver*
