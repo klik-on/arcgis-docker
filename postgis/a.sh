090bd8e83c6bc66e39d32732922bd0a480d680d6
klik-on <123057191+klik-on@users.noreply.github.com>
2023-07-14 07:44:50 +0700

    Add files via upload

---
+++ postgis/a.sh
@@ -0,0 +1,9 @@
+# Cek Lokasi lib PostgreSQL
+# pg_config --pkglibdir
+# /usr/lib/postgresql/12/lib
+# chmod 644 /usr/lib/postgresql/12/lib/st_geometry.so
+
+for pg_ags in ./lib/*.so; do
+  docker cp $pg_ags PGSQL-12:/usr/lib/postgresql/12/lib 
+done
+
