9dc29785fda9cd4351000eee173555dcb09e8956
klik-on <123057191+klik-on@users.noreply.github.com>
2023-07-14 07:47:57 +0700

    Update a.sh

--- postgis/a.sh
+++ postgis/a.sh
@@ -1,7 +1,7 @@
 # Cek Lokasi lib PostgreSQL
 # pg_config --pkglibdir
 # /usr/lib/postgresql/12/lib
-# chmod 644 /usr/lib/postgresql/12/lib/st_geometry.so
+# chmod 644 /usr/lib/postgresql/12/lib/{st_geometry.so PGSQLEngine.so}
 
 # lib ArcGIS Desktop 10.8.2
 for pg_ags in ./lib/*.so; do
