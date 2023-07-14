efc154638804d94c56e0ba703db8362e61d1dfb4
klik-on <123057191+klik-on@users.noreply.github.com>
2023-07-14 07:38:31 +0700

    Update a.sh

--- pg-12/a.sh
+++ pg-12/a.sh
@@ -1,4 +1,8 @@
+# Cek Lokasi lib PostgreSQL
+# pg_config --pkglibdir
+# /usr/lib/postgresql/12/lib
+# chmod 644 /usr/lib/postgresql/12/lib/st_geometry.so
+
 for pg_ags in ./lib/*.so; do
   docker cp $pg_ags PGSQL-12:/opt/bitnami/postgresql/lib
 done
-
