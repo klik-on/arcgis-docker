97f516c02eb70a1cc2d443beab74996a52f20a40
klik-on <123057191+klik-on@users.noreply.github.com>
2023-10-13 20:54:12 +0700

    Update updt_tomcat.sh

--- arcgis-server/patch/updt_tomcat.sh
+++ arcgis-server/patch/updt_tomcat.sh
@@ -1,5 +1,6 @@
 # docker exec -it SERVER /bin/bash
 # wget https://raw.githubusercontent.com/klik-on/arcgis-docker/main/arcgis-server/patch/updt_tomcat.sh
+# docker restart {SERVER,PORTAL,DATASTORE}
 
 /home/arcgis/server/framework/runtime/tomcat/bin/version.sh
 
