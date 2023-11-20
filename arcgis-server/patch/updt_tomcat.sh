e25daae395f8d87c1ae215962723ea49b2bab0c3
klik-on <123057191+klik-on@users.noreply.github.com>
2023-11-20 07:40:48 +0700

    Update updt_tomcat.sh

--- arcgis-server/patch/updt_tomcat.sh
+++ arcgis-server/patch/updt_tomcat.sh
@@ -4,7 +4,7 @@
 
 /home/arcgis/server/framework/runtime/tomcat/bin/version.sh
 
-tver='9.0.82'
+tver='9.0.83'
 cd /tmp
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v$tver/bin/apache-tomcat-$tver.tar.gz
 tar -xvf apache-tomcat-$tver.tar.gz
