58c4d439839a5453414cb7cb7118c8d47ba64498
klik-on <123057191+klik-on@users.noreply.github.com>
2023-12-08 13:27:30 +0700

    Update updt_tomcat.sh

--- web-adaptor/updt_tomcat.sh
+++ web-adaptor/updt_tomcat.sh
@@ -1,6 +1,6 @@
 # docker exec -it WEBADAPTOR /bin/bash
 
-cd temp
+cd /tmp
 tver='9.0.83'
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v$tver/bin/apache-tomcat-$tver.tar.gz
 tar -xvf apache-tomcat-$tver.tar.gz
@@ -8,10 +8,10 @@ cd /usr/local/tomcat
 ./bin/version.sh
 cp -r bin bin_bak
 cp -r lib lib_bak
-cp -fv ./temp/apache-tomcat-$tver/bin/*.jar ./bin
-cp -fv ./temp/apache-tomcat-$tver/lib/*.jar ./lib
+cp -fv /tmp/apache-tomcat-$tver/bin/*.jar ./bin
+cp -fv /tmp/apache-tomcat-$tver/lib/*.jar ./lib
 ./bin/version.sh
-rm -rf ./temp/apache-tomcat-$tver*
+rm -rf /tmp/apache-tomcat-$tver*
 
 # exit
 # docker restart WEBADAPTOR
