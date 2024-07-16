77dd88595ae19f58a9b8b6947476463d576bbecb
klik-on <123057191+klik-on@users.noreply.github.com>
2024-07-16 14:03:35 +0700

    Update updt_tomcat.sh

--- web-adaptor/updt_tomcat.sh
+++ web-adaptor/updt_tomcat.sh
@@ -2,7 +2,7 @@
 # wget https://raw.githubusercontent.com/klik-on/arcgis-docker/main/web-adaptor/updt_tomcat.sh
 
 cd /tmp
-tver='9.0.84'
+tver='9.0.91'
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v$tver/bin/apache-tomcat-$tver.tar.gz
 tar -xvf apache-tomcat-$tver.tar.gz
 cd /usr/local/tomcat
