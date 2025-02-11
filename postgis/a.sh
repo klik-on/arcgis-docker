7a5adcbd1c641d6b9da0ea9f68e466f9fb7b86ac
klik-on <123057191+klik-on@users.noreply.github.com>
2025-02-11 15:25:12 +0700

    Update a.sh

--- postgis/a.sh
+++ postgis/a.sh
@@ -9,7 +9,7 @@
 # chown root. /usr/lib/postgresql/15/lib/st_geometry.so
 # ls -l /usr/lib/postgresql/15/lib/st_geometry.so
 
-# lib ArcGIS Desktop 10.8.2
+# lib ArcGIS Desktop 10.8.2 ==> POSTGRESQL 12
 for pg_ags in ./lib/*.so; do
   docker cp $pg_ags PGIS-15:/usr/lib/postgresql/15/lib 
 done
