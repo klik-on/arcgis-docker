15ddcaea5c582ee7969fecf6b115257460abacec
klik-on <123057191+klik-on@users.noreply.github.com>
2025-05-23 11:07:18 +0700

    Update updt_tomcat.sh

--- datastore/patch/updt_tomcat.sh
+++ datastore/patch/updt_tomcat.sh
@@ -2,7 +2,7 @@
 # docker restart {SERVER,PORTAL,DATASTORE}
  /home/arcgis/datastore/framework/runtime/tomcat/bin/version.sh
 
-tver='9.0.104'
+tver='9.0.105'
 cd /tmp
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v$tver/bin/apache-tomcat-$tver.tar.gz
 tar -xvf apache-tomcat-$tver.tar.gz
