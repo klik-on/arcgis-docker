dd25971e8c013b4a3de9adbcc7882aca37fc95cf
klik-on <123057191+klik-on@users.noreply.github.com>
2025-09-08 11:21:07 +0700

    Update cek_index_idx.sql

--- postgis/gdal/cek_index_idx.sql
+++ postgis/gdal/cek_index_idx.sql
@@ -6,3 +6,13 @@ FROM
 WHERE
     schemaname = 'datagis'
     AND tablename = 'ADM_KAB_KOTA';
+
+
+## CEK -- Output Index Scan using "ADM_KAB_KOTA_geom_idx" on "ADM_KAB_KOTA"  
+EXPLAIN
+SELECT * 
+FROM datagis."ADM_KAB_KOTA"
+WHERE ST_Intersects(
+  geom,
+  ST_GeomFromText('POLYGON((115.0 -8.5, 115.2 -8.5, 115.2 -8.3, 115.0 -8.3, 115.0 -8.5))', 4326)
+);
