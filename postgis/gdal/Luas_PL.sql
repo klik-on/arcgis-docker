2cd363ee193c36378a1de73a7fa80c1b461ea495
klik-on <123057191+klik-on@users.noreply.github.com>
2025-09-09 10:45:20 +0700

    Update Luas_PL.sql

--- postgis/gdal/Luas_PL.sql
+++ postgis/gdal/Luas_PL.sql
@@ -71,3 +71,50 @@ SELECT * FROM Gabungan
 ORDER BY
   CASE WHEN pl2024_id = 'TOTAL' THEN 1 ELSE 0 END,
   pl2024_id;
+
+
+
+-- Hitung luas per pl2024_id di seluruh wilayah (tanpa filter), ditambah deskripsi dari KODE_PL dan provinsi akan LAMA
+WITH Luaspl AS (
+  SELECT
+    a.pl2024_id::text AS pl2024_id,
+    b."WADMPR",
+    SUM(
+      ST_Area(
+        ST_Transform(ST_Intersection(a.geom, b.geom), 54034)
+      )
+    ) / 10000 AS "LUAS_CEA_HA"
+  FROM 
+    datagis."PL2024_AR_250K" a
+  JOIN 
+    datagis."ADM_KAB_KOTA" b
+    ON a.geom && b.geom AND ST_Intersects(a.geom, b.geom)
+  GROUP BY 
+    a.pl2024_id, b."WADMPR"
+),
+Luaspl_dengan_ket AS (
+  SELECT
+    l.pl2024_id,
+    l."WADMPR",
+    k."DESKRIPSI_PL",
+    l."LUAS_CEA_HA"
+  FROM
+    Luaspl l
+  LEFT JOIN
+    datagis."KODE_PL" k ON l.pl2024_id = k."KD_PL"::text
+),
+Gabungan AS (
+  SELECT * FROM Luaspl_dengan_ket
+  UNION ALL
+  SELECT
+    'TOTAL' AS pl2024_id,
+    NULL AS "WADMPR",
+    NULL AS "DESKRIPSI_PL",
+    SUM("LUAS_CEA_HA") AS "LUAS_CEA_HA"
+  FROM Luaspl_dengan_ket
+)
+
+SELECT * FROM Gabungan
+ORDER BY
+  CASE WHEN pl2024_id = 'TOTAL' THEN 1 ELSE 0 END,
+  pl2024_id;
