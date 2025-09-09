e053c34be417f4deffb17123ccbdae91b61680d8
klik-on <123057191+klik-on@users.noreply.github.com>
2025-09-09 10:29:55 +0700

    Update Luas_PL.sql

--- postgis/gdal/Luas_PL.sql
+++ postgis/gdal/Luas_PL.sql
@@ -26,3 +26,48 @@ SELECT * FROM Gabungan
 ORDER BY
   CASE WHEN pl2020_id = 'TOTAL' THEN 1 ELSE 0 END,
   pl2020_id;
+
+
+-- Hitung luas per pl2024_id di wilayah Bali, ditambah deskripsi dari KODE_PL
+WITH Luaspl AS (
+  SELECT
+    a.pl2024_id::text AS pl2024_id,
+    SUM(
+      ST_Area(
+        ST_Transform(ST_Intersection(a.geom, b.geom), 54034)
+      )
+    ) / 10000 AS "LUAS_CEA_HA"
+  FROM 
+    datagis."PL2024_AR_250K" a
+  JOIN 
+    datagis."ADM_KAB_KOTA" b
+    ON ST_Intersects(a.geom, b.geom)
+  WHERE 
+    b."WADMPR" = 'Bali'
+  GROUP BY 
+    a.pl2024_id
+),
+Luaspl_dengan_ket AS (
+  SELECT
+    l.pl2024_id,
+    k."DESKRIPSI_PL",
+    l."LUAS_CEA_HA"
+  FROM
+    Luaspl l
+  LEFT JOIN
+    datagis."KODE_PL" k ON l.pl2024_id = k."KD_PL"::text
+),
+Gabungan AS (
+  SELECT * FROM Luaspl_dengan_ket
+  UNION ALL
+  SELECT
+    'TOTAL' AS pl2024_id,
+    NULL AS "DESKRIPSI_PL",
+    SUM("LUAS_CEA_HA") AS "LUAS_CEA_HA"
+  FROM Luaspl_dengan_ket
+)
+
+SELECT * FROM Gabungan
+ORDER BY
+  CASE WHEN pl2024_id = 'TOTAL' THEN 1 ELSE 0 END,
+  pl2024_id;
