1c7868fcba719d6d353f6a50ac8aa337fa74a1d9
klik-on <123057191+klik-on@users.noreply.github.com>
2025-09-12 10:11:16 +0700

    Update VALIDASI_GEOM.sql

--- postgis/gdal/VALIDASI_GEOM.sql
+++ postgis/gdal/VALIDASI_GEOM.sql
@@ -17,6 +17,12 @@ WHERE geom IS NOT NULL
 
 -- Cek Valid geometri, pastikan semua geometri valid: Data count 0
 SELECT COUNT(*) FROM datagis."KWSHUTAN_AR_250K" WHERE NOT ST_IsValid(geom);
+-- atau
+SELECT COUNT(*)
+FROM datagis."KWSHUTAN_AR_250K"
+WHERE NOT ST_IsValid(geom)
+   OR GeometryType(geom) != 'MULTIPOLYGON';
+
 
 -- CEK  pastikan semua bertipe MultiPolygon
 SELECT DISTINCT GeometryType(geom) FROM datagis."KWSHUTAN_AR_250K";
