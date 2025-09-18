e1aaeefd61ad611207007e3c2dc534eecd37ac93
klik-on <123057191+klik-on@users.noreply.github.com>
2025-09-18 15:05:18 +0700

    Create multiline.sh

---
+++ postgis/gdal/api_to_ogr/multiline.sh
@@ -0,0 +1,37 @@
+
+#!/bin/bash
+set -e
+
+# === Konfigurasi ===
+DB_HOST="172.16.2.122"
+DB_PORT="5432"
+DB_NAME="postgres"
+DB_USER="postgres.67888"
+DB_PASS="password00"
+SCHEMA="dss"
+
+DATA_DIR="/app/data/dss"
+ZIP_FILE="BTS_LAUT_TERITORIAL.gdb.zip"
+TABLE_NAME="BTS_LAUT_TERITORIAL"
+ZIP_PATH="${DATA_DIR}/${ZIP_FILE}"
+
+# === Impor ke PostgreSQL ===
+echo "?? Mengimpor ${GDB_FOLDER} ke PostgreSQL..."
+ogr2ogr -f "PostgreSQL" \
+  PG:"host=${DB_HOST} port=${DB_PORT} dbname=${DB_NAME} user=${DB_USER} password=${DB_PASS}" \
+  "$ZIP_PATH" \
+  -nlt MULTILINESTRING \
+  -nln "${TABLE_NAME}" \
+  -dim 2 \
+  -lco SCHEMA=${SCHEMA} \
+  -lco GEOMETRY_NAME=geom \
+  -lco FID=id \
+  -lco SPATIAL_INDEX=GIST \
+  -lco LAUNDER=NO \
+  -lco OVERWRITE=YES \
+  -t_srs EPSG:4326 \
+  -progress \
+  --config OGR_OPENFILEGDB_METHOD SKIP \
+  --config OGR_ENABLE_CURVE_REDUCTION YES
+
+echo "? Impor selesai."
