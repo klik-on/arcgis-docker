4997256df6e56553c351ee83e7aff8d7b083c70c
klik-on <123057191+klik-on@users.noreply.github.com>
2025-10-09 08:20:43 +0700

    Update LUAS_KWS_NO_ADM.sql

--- postgis/gdal/LUAS_KWS_NO_ADM.sql
+++ postgis/gdal/LUAS_KWS_NO_ADM.sql
@@ -5,34 +5,35 @@ SELECT
   c."FUNGSI_KWS", 
   SUM(
     ST_Area(
-      ST_Transform(a.geom, 54034)   -- Proyeksi  ESRI CEA dengan satuan meter
+      ST_Transform(a.geom, 54034)   -- Proyeksi ESRI CEA (meter)
     )
-  ) / 10000 AS "LUAS_CEA_HA"        -- Total luas dalam hektar
-FROM 
-  datagis."KWSHUTAN_AR_250K" a        -- Tabel Data IGT KWS
-LEFT JOIN 
-  kodefikasi."KODE_KWS" c                        -- Tabel Kodefikasi KODE_KWS
-    ON a."FUNGSIKWS" = c."KD_KWS"
-WHERE 
-  a.geom IS NOT NULL                   -- hanya geometri yang valid (tidak kosong) yang dihitung
-GROUP BY 
-  a."FUNGSIKWS", c."KD_KWS", c."FUNGSI_KWS", c."NOURUT_KWS"
-UNION ALL
-SELECT 
-  NULL AS "NOURUT_KWS", 
-  NULL AS "KODEKWS", 
-  'Grand Total' AS "FUNGSI_KWS",   -- Mengganti kolom ini dengan Grand Total sebagai string
-  SUM(
-    ST_Area(
-      ST_Transform(a.geom, 54034)   -- Proyeksi  ESRI CEA dengan satuan meter
-    )
-  ) / 10000 AS "LUAS_CEA_HA"        -- Total luas dalam hektar
+  ) / 10000 AS "LUAS_CEA_HA"        -- Konversi ke hektar
 FROM 
   datagis."KWSHUTAN_AR_250K" a
 LEFT JOIN 
-  kodefikasi."KODE_KWS" c 
+  kodefikasi."KODE_KWS" c
     ON a."FUNGSIKWS" = c."KD_KWS"
 WHERE 
   a.geom IS NOT NULL
+GROUP BY 
+  c."NOURUT_KWS", a."FUNGSIKWS", c."FUNGSI_KWS"
+
+UNION ALL
+
+-- Grand Total
+SELECT 
+  NULL AS "NOURUT_KWS", 
+  NULL AS "KODEKWS", 
+  'Grand Total' AS "FUNGSI_KWS", 
+  SUM(
+    ST_Area(
+      ST_Transform(a.geom, 54034)
+    )
+  ) / 10000 AS "LUAS_CEA_HA"
+FROM 
+  datagis."KWSHUTAN_AR_250K" a
+WHERE 
+  a.geom IS NOT NULL
+
 ORDER BY 
-  "NOURUT_KWS" NULLS FIRST;         -- Menempatkan Grand Total di bagian bawah
+  "NOURUT_KWS" NULLS FIRST;
