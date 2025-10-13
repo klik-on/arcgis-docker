26240bb99a3f778ccd7e8e6f47a2aaf8da64022d
klik-on <123057191+klik-on@users.noreply.github.com>
2025-10-13 15:09:06 +0700

    Update Luas_PL_2020_2024_Bogor.sql

--- postgis/gdal/Luas_PL_2020_2024_Bogor.sql
+++ postgis/gdal/Luas_PL_2020_2024_Bogor.sql
@@ -9,7 +9,7 @@ LuasPL2020 AS (
       ST_Area(
         ST_Transform(ST_Intersection(a.geom, adm.geom), 54034)
       )
-    ) / 10000 AS luas_2020
+    ) / 10000 AS pl_2020
   FROM datagis."PL2020_AR_250K" a
   INNER JOIN datagis."ADM_KAB_KOTA" adm ON ST_Intersects(a.geom, adm.geom)
   WHERE adm."WADMKK" = 'Bogor'
@@ -24,7 +24,7 @@ LuasPL2021 AS (
       ST_Area(
         ST_Transform(ST_Intersection(b.geom, adm.geom), 54034)
       )
-    ) / 10000 AS luas_2021
+    ) / 10000 AS pl_2021
   FROM datagis."PL2021_AR_250K" b
   INNER JOIN datagis."ADM_KAB_KOTA" adm ON ST_Intersects(b.geom, adm.geom)
   WHERE adm."WADMKK" = 'Bogor'
@@ -39,7 +39,7 @@ LuasPL2022 AS (
       ST_Area(
         ST_Transform(ST_Intersection(c.geom, adm.geom), 54034)
       )
-    ) / 10000 AS luas_2022
+    ) / 10000 AS pl_2022
   FROM datagis."PL2022_AR_250K" c
   INNER JOIN datagis."ADM_KAB_KOTA" adm ON ST_Intersects(c.geom, adm.geom)
   WHERE adm."WADMKK" = 'Bogor'
@@ -54,7 +54,7 @@ LuasPL2023 AS (
       ST_Area(
         ST_Transform(ST_Intersection(d.geom, adm.geom), 54034)
       )
-    ) / 10000 AS luas_2023
+    ) / 10000 AS pl_2023
   FROM datagis."PL2023_AR_250K" d
   INNER JOIN datagis."ADM_KAB_KOTA" adm ON ST_Intersects(d.geom, adm.geom)
   WHERE adm."WADMKK" = 'Bogor'
@@ -69,7 +69,7 @@ LuasPL2024 AS (
       ST_Area(
         ST_Transform(ST_Intersection(e.geom, adm.geom), 54034)
       )
-    ) / 10000 AS luas_2024
+    ) / 10000 AS pl_2024
   FROM datagis."PL2024_AR_250K" e
   INNER JOIN datagis."ADM_KAB_KOTA" adm ON ST_Intersects(e.geom, adm.geom)
   WHERE adm."WADMKK" = 'Bogor'
@@ -80,11 +80,11 @@ GabunganPivot AS (
   SELECT
     COALESCE(p20.pl_id, p21.pl_id, p22.pl_id, p23.pl_id, p24.pl_id) AS pl_id,
     COALESCE(p20."WADMKK", p21."WADMKK", p22."WADMKK", p23."WADMKK", p24."WADMKK") AS wadmkk,
-    p20.luas_2020,
-    p21.luas_2021,
-    p22.luas_2022,
-    p23.luas_2023,
-    p24.luas_2024
+    p20.pl_2020,
+    p21.pl_2021,
+    p22.pl_2022,
+    p23.pl_2023,
+    p24.pl_2024
   FROM LuasPL2020 p20
   FULL OUTER JOIN LuasPL2021 p21 ON p20.pl_id = p21.pl_id AND p20."WADMKK" = p21."WADMKK"
   FULL OUTER JOIN LuasPL2022 p22 ON COALESCE(p20.pl_id, p21.pl_id) = p22.pl_id AND COALESCE(p20."WADMKK", p21."WADMKK") = p22."WADMKK"
@@ -98,11 +98,11 @@ GabunganFinal AS (
   SELECT
     'TOTAL' AS pl_id,
     'Bogor' AS wadmkk,
-    SUM(luas_2020),
-    SUM(luas_2021),
-    SUM(luas_2022),
-    SUM(luas_2023),
-    SUM(luas_2024)
+    SUM(pl_2020),
+    SUM(pl_2021),
+    SUM(pl_2022),
+    SUM(pl_2023),
+    SUM(pl_2024)
   FROM GabunganPivot
 )
 
