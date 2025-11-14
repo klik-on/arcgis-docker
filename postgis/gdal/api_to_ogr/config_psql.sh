8a7cf564b1803c690d043d4b6a1e7895448b217b
klik-on <123057191+klik-on@users.noreply.github.com>
2025-11-14 07:42:46 +0700

    Update config_psql.sh

--- postgis/gdal/api_to_ogr/config_psql.sh
+++ postgis/gdal/api_to_ogr/config_psql.sh
@@ -16,8 +16,7 @@ echo "==> Mengubah parameter konfigurasi PostgreSQL..."
 docker exec "$CONTAINER" sed -i "s/^#*shared_buffers.*/shared_buffers = 8GB/" "$CONF"
 docker exec "$CONTAINER" sed -i "s/^#*work_mem.*/work_mem = 256MB/" "$CONF"
 docker exec "$CONTAINER" sed -i "s/^#*maintenance_work_mem.*/maintenance_work_mem = 1GB/" "$CONF"
-docker exec "$CONTAINER" sed -i "s/^#*max_parallel_workers_per_gather.*/max_parallel_workers_per_gather = 8/
-" "$CONF"
+docker exec "$CONTAINER" sed -i "s/^#*max_parallel_workers_per_gather.*/max_parallel_workers_per_gather = 8/" "$CONF"
 docker exec "$CONTAINER" sed -i "s/^#*temp_buffers.*/temp_buffers = 512MB/" "$CONF"
 
 echo "==> Restart container Supabase DB..."
