d4dd3bbce0cbbb630ddbc71c50b4315b83e5df6e
klik-on <123057191+klik-on@users.noreply.github.com>
2026-04-09 08:17:27 +0700

    Update ap_wa.sh

--- web-adaptor/ap_wa.sh
+++ web-adaptor/ap_wa.sh
@@ -4,6 +4,6 @@
 
 WA_HOSTNAME=${HOSTNAME:-portal.domain.go.id}
 /arcgis/webadaptor10.9.1/java/tools/configurewebadaptor.sh -m portal \
- -w https://${HOSTNAME}/portal/webadaptor \
+ -w https://${WA_HOSTNAME}/portal/webadaptor \
  -g https://portal.agis.lokal:7443 -u portaladmin -p test2026
 
