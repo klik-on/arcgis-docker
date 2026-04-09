919e5cc0be38af2c45a5cbd3e0051b10407382e7
klik-on <123057191+klik-on@users.noreply.github.com>
2026-04-09 08:18:59 +0700

    Update as_wa.sh

--- web-adaptor/as_wa.sh
+++ web-adaptor/as_wa.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+# Menggunakan hostname dari environment atau default ke dbgis
+# docker exec -it WEBADAPTOR /usr/local/bin/ap_wa.sh
+
+WA_HOSTNAME=${HOSTNAME:-server.domain.go.id}
 /arcgis/webadaptor10.9.1/java/tools/configurewebadaptor.sh -m server \
- -w https://${HOSTNAME}/server/webadaptor \
- -g https://SERVER.WEBGIS.LOKAL:6443 \
- -u siteadmin -p test2022 -a true
+ -w https://${WA_HOSTNAME}/server/webadaptor \
+ -g https://server.agis.lokalL:6443 \
+ -u siteadmin -p test2026 -a true
