e5ff9ddd49718606e33c8a0aa1971333f8264b4f
klik-on <123057191+klik-on@users.noreply.github.com>
2026-04-09 08:16:35 +0700

    Update ap_wa.sh

--- web-adaptor/ap_wa.sh
+++ web-adaptor/ap_wa.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# Menggunakan hostname dari environment atau default ke dbgis
+# docker exec -it WEBADAPTOR /usr/local/bin/ap_wa.sh
+
+WA_HOSTNAME=${HOSTNAME:-portal.domain.go.id}
 /arcgis/webadaptor10.9.1/java/tools/configurewebadaptor.sh -m portal \
  -w https://${HOSTNAME}/portal/webadaptor \
- -g https://PORTAL.WEBGIS.LOKAL:7443 -u portaladmin -p test2022
+ -g https://portal.agis.lokal:7443 -u portaladmin -p test2026
+#!/bin/bash
